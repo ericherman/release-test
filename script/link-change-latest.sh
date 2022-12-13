d9d9fb7adae933a0802838544dd876685a6c1404
Eric Herman <eric@freesa.org>
2022-12-13 13:20:24 +0100

    make the script pull version from environment
    If GitHub, then use GITHUB_REF, otherwise try `git describe --tags`
    
    Co-authored-by: Jan Ainali <jan@publiccode.net>

--- script/link-change-latest.sh
+++ script/link-change-latest.sh
@@ -10,5 +10,24 @@ echo
 
 set -x
 
-ln -sv changelog/$(ls -1 changelog | sort | tail -n1) \
-	release.body
+if [ "_${RELEASE_NOTES_FILE}_" == "__" ] || [ ! -f "${RELEASE_NOTES_FILE}" ];
+then
+	if [ "_${GITHUB_REF}_" != "__" ]; then
+		TAG=$( echo "${GITHUB_REF}" | sed -e's@refs/tags/@@' )
+	else
+		# we are running locally or not in a GITHUB environment
+		GIT_HASH=$( git log -n1 --pretty='%h' )
+		#TODO: not safe for multiple tags
+		TAG=$( git describe --exact-match --tags ${GIT_HASH} )
+	fi
+	if [ "_${TAG}_" != "__" ]; then
+		RELEASE_NOTES_FILE=changelog/changelog-${TAG}.md
+	fi
+fi
+
+if [ "_${RELEASE_NOTES_FILE}_" == "__" ] || [ ! -f "${RELEASE_NOTES_FILE}" ];
+then
+	RELEASE_NOTES_FILE=changelog/$( ls -1 changelog | sort | tail -n1 )
+fi
+
+ln -svf "$RELEASE_NOTES_FILE" release.body
